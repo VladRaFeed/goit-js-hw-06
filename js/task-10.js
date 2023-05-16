0ade54eeeb399ba330f3717b7a22f3bdf6fba699
unknown <jack22toplolkek@gmail.com>
2023-05-16 21:38:42 +0300

    last task complete

--- js/task-10.js
+++ js/task-10.js
@@ -5,3 +5,36 @@ function getRandomHexColor() {
 }
 
 const allBoxesParent = document.querySelector("#controls");
+const boxesList = document.querySelector("#boxes");
+const inputEl = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+// console.log(allBoxesParent);
+// console.log(boxesList);
+// console.log(createButton);
+// console.log(destroyButton);
+// console.log(inputEl);
+createButton.addEventListener("click", createBoxes);
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes() {
+  const amount = Number(inputEl.value);
+  clearBoxes();
+  let size = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesList.appendChild(box);
+    size += 10;
+  }
+}
+
+function destroyBoxes() {
+  clearBoxes();
+}
+
+function clearBoxes() {
+  boxesList.innerHTML = "";
+}
